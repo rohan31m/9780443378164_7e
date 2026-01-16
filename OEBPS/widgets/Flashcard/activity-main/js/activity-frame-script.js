c38bdfb0620c02797eaf42e9f1d55d2b8a644147
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2026-01-16 20:37:42 +0530

    Changes to avoid scroll and adjust label positions.

--- OEBPS/widgets/Flashcard/activity-main/js/activity-frame-script.js
+++ OEBPS/widgets/Flashcard/activity-main/js/activity-frame-script.js
@@ -29,6 +29,8 @@ function resize(pageHolder, style) {
     var newWidth = scale * pageHolderWidth;
     var leftPos = (window.innerWidth - newWidth) / 2;
     //scale = Number(scale.split(','))[0];
+    //if(scale>1) scale=1;
+
     pageHolder.setAttribute('style', style + ' ' + '-ms-transform-origin: left top; -webkit-transform-origin: left top; -moz-transform-origin: left top; -o-transform-origin: left top; transform-origin: left top; -ms-transform: scale(' + scale + '); -webkit-transform: scale3d(' + scale + ', 1); background: #FFFFFF; position:absolute; top:0px; left:' + leftPos + 'px; -moz-transform: scale(' + scale + '); -o-transform: scale(' + scale + '); transform: scale(' + scale + '); background-color:transparent;');
     //pageHolder.setAttribute('style', style + ' ' + '-ms-transform: scale(' + scale + '); -webkit-transform: scale3d(' + scale + ', 1); -moz-transform: scale(' + scale + '); -o-transform: scale(' + scale + '); transform: scale(' + scale + '); background-color:transparent;');
     //console.log(((jqnc("body").width() - jqnc("#Menu").width()) / 2) + "px");	        
